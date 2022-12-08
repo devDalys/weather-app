fdb4360da1f15cd83277ed5645c9fae2879b4520
Victor Vinogradov <v.vinogradov@vvinogradov.local>
2022-12-08 18:03:22 +0300

    404 page

--- src/components/PageNotFound/PageNotFound.tsx
+++ src/components/PageNotFound/PageNotFound.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
 import "../WeatherContainer/WeatherContainer.css";
+import {useTranslation} from "react-i18next";
+import {Link} from "react-router-dom";
+
 const PageNotFound: React.FC = () => {
-  return (
-    <div className="container">
-      <span className="container__title">404</span>
-      <br />
-      <br />
-      <br />
-      <h1 className="container__title"> Page not found</h1>
-    </div>
-  );
+    const {t} = useTranslation('translation')
+
+    return (
+        <div className="container" style={{justifyContent:'center'}}>
+            <span className="container__title">404</span>
+            <h1 className="container__title"> {t('Page not found')}</h1>
+            <Link to={'/'}>
+                <a className='button'>На главную</a>
+            </Link>
+        </div>
+    );
 };
 
 export default PageNotFound;
