3184d28221df04cf7d19cdf7f5045bb5c5e9168b
Виктор Виноградов <idalys1357@gmail.com>
2023-01-19 21:34:14 +0300

    Added translates

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,45 +3,47 @@ import css from "./Settings.module.css";
 import { useNavigate } from "react-router-dom";
 import { Paths } from "../../types/types";
 import { LegacyRef } from "react";
+import { useTranslation } from "react-i18next";
 const Settings: React.FC = () => {
   const navigate = useNavigate();
   const clearRef = React.useRef<HTMLButtonElement | null>(null);
+  const { t } = useTranslation("Translation");
   const clearAll = () => {
     localStorage.clear();
     sessionStorage.clear();
 
-      if(clearRef.current){
-          clearRef.current.classList.add(css.animation)
-          setTimeout(() => {
-              clearRef.current?.classList.remove(css.animation)
-          },1500)
-      }
+    if (clearRef.current) {
+      clearRef.current.classList.add(css.animation);
+      setTimeout(() => {
+        clearRef.current?.classList.remove(css.animation);
+      }, 1500);
+    }
   };
 
   return (
     <>
-      <h2 className={css.title}>Настройки</h2>
+      <h2 className={css.title}>{t("Settings")}</h2>
 
       <button onClick={() => navigate(Paths.Start)} className={css.button}>
-        Редактировать города
+        {t("Change cities")}
       </button>
       <button ref={clearRef} className={css.button} onClick={() => clearAll()}>
-        Очистить все данные
+        {t("Clear data")}
       </button>
-      <button className={css.button}>Фон другого сезона</button>
+      <button className={css.button}>{t("Change background")}</button>
       <a
         href={"https://github.com/devDalys/weather-app"}
         target={"_blank"}
         className={css.button}
       >
-        Ссылка на GitHub
+        {t("Link GitHub")}
       </a>
       <a
         href={"http://devdalys.netlify.app"}
         target={"_blank"}
         className={css.button}
       >
-        Об авторе
+        {t("About")}
       </a>
     </>
   );
